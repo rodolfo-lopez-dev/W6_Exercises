e5e75529922f5264209bb3fe3805f298487a5fe5
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-31 13:07:50 -0500

    completed simple functions

---
+++ Functions/simple_functions.py
@@ -0,0 +1,23 @@
+# Author: Rodolfo 
+# Date: October 31
+# Script will def three functions
+
+# Name favortie movie, band
+def favorite_things(name, movie, band):
+    print("Name:", name)
+    print("Favorite Movie:", movie)
+    print("Favorite Band:", band)
+
+# why I did data analyst program
+def why_im_here():
+    print("I joined this program because it will equip me with the resources and knowledge I need to thrive in this rapidly evolving field.")
+
+# describe favorite place 
+def favorite_place(place, reason):
+    print(f"One of my favorite places to visit is {place} because {reason}.")
+
+# favorite place to visit
+favorite_things ("Rodolfo","Break at Tiffany's","The Strokes")
+why_im_here()
+favorite_place("The Netherlands", "it was a such a beautiful and culurally diverse country")
+favorite_place("Chicago", "being able to explore the entire Chicago area has been a blessing in disguise")
